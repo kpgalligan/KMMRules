dd84348b026b5b69d7fa7d632e82b0ff8d9daec2
Kevin Galligan <kgalligan@gmail.com>
2022-12-27 14:42:24 -0500

    KMM SPM package release for 0.6.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KMMBridgeKickStart/co/touchlab/kmmbridgekickstart/allshared-kmmbridge/0.13.0/allshared-kmmbridge-0.13.0.zip"
-let remoteKotlinChecksum = "fe57822fa8ae5806e791558b1a632c3cd3af6f185d8e866ff4e132f40f68a6d4"
+let remoteKotlinUrl = "https://maven.pkg.github.com/kpgalligan/KMMRules/kpgalligan/github/kmmrules/allshared-kmmbridge/0.6.1/allshared-kmmbridge-0.6.1.zip"
+let remoteKotlinChecksum = "cb65c0e92353deef46d426bf4a56815b56bc07b2b2858fe0a139de30823622ea"
 let packageName = "allshared"
 
 let package = Package(
