6dfd3b081847a62b50b1bd9c3697f1550e0315cc
Kevin Galligan <kgalligan@gmail.com>
2022-12-27 16:47:54 -0500

    KMM SPM package release for 0.8.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/kpgalligan/KMMRules/kpgalligan/github/kmmrules/allshared-kmmbridge/0.7.2/allshared-kmmbridge-0.7.2.zip"
-let remoteKotlinChecksum = "7ea80a289cb65508ed3abd8e6e074bf42ec63c96ad497fff63d2df0563d0c762"
+let remoteKotlinUrl = "https://maven.pkg.github.com/kpgalligan/KMMRules/kpgalligan/github/kmmrules/allshared-kmmbridge/0.8.1/allshared-kmmbridge-0.8.1.zip"
+let remoteKotlinChecksum = "770b6365168db4c0e1e4b430906dead81cc6911697f85c7a1dac8d8ecaffa606"
 let packageName = "allshared"
 
 let package = Package(
